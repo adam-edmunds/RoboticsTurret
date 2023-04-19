07099a4af0f6a614471218819b35298a0fe60298
Adam Edmunds <mightylordx786@gmail.com>
2023-04-19 08:03:47 +0100

    init commit

---
+++ MyLaserTurret/MyLaserTurret/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MyLaserTurret
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.GrabLabel = new System.Windows.Forms.Label();
+            this.grabBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cameraCollection = new System.Windows.Forms.ComboBox();
+            this.cameraButton = new System.Windows.Forms.Button();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.xText = new System.Windows.Forms.TextBox();
+            this.yText = new System.Windows.Forms.TextBox();
+            this.modeCombo = new System.Windows.Forms.ComboBox();
+            this.modeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // port
+            // 
+            this.port.PortName = "COM3";
+            // 
+            // GrabLabel
+            // 
+            this.GrabLabel.AutoSize = true;
+            this.GrabLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GrabLabel.Location = new System.Drawing.Point(279, 101);
+            this.GrabLabel.Name = "GrabLabel";
+            this.GrabLabel.Size = new System.Drawing.Size(185, 33);
+            this.GrabLabel.TabIndex = 4;
+            this.GrabLabel.Text = "isGrabbing: ";
+            // 
+            // grabBox
+            // 
+            this.grabBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grabBox.Location = new System.Drawing.Point(454, 96);
+            this.grabBox.Name = "grabBox";
+            this.grabBox.Size = new System.Drawing.Size(168, 44);
+            this.grabBox.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(656, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 360);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cameraCollection
+            // 
+            this.cameraCollection.FormattingEnabled = true;
+            this.cameraCollection.Location = new System.Drawing.Point(656, 23);
+            this.cameraCollection.Name = "cameraCollection";
+            this.cameraCollection.Size = new System.Drawing.Size(380, 21);
+            this.cameraCollection.TabIndex = 11;
+            // 
+            // cameraButton
+            // 
+            this.cameraButton.Location = new System.Drawing.Point(1043, 20);
+            this.cameraButton.Name = "cameraButton";
+            this.cameraButton.Size = new System.Drawing.Size(174, 23);
+            this.cameraButton.TabIndex = 12;
+            this.cameraButton.Text = "detect";
+            this.cameraButton.UseVisualStyleBackColor = true;
+            this.cameraButton.Click += new System.EventHandler(this.cameraButton_Click);
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xLabel.Location = new System.Drawing.Point(279, 177);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(53, 33);
+            this.xLabel.TabIndex = 13;
+            this.xLabel.Text = "X: ";
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yLabel.Location = new System.Drawing.Point(279, 220);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(54, 33);
+            this.yLabel.TabIndex = 14;
+            this.yLabel.Text = "Y: ";
+            // 
+            // xText
+            // 
+            this.xText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xText.Location = new System.Drawing.Point(338, 166);
+            this.xText.Name = "xText";
+            this.xText.Size = new System.Drawing.Size(168, 44);
+            this.xText.TabIndex = 15;
+            // 
+            // yText
+            // 
+            this.yText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yText.Location = new System.Drawing.Point(338, 215);
+            this.yText.Name = "yText";
+            this.yText.Size = new System.Drawing.Size(168, 44);
+            this.yText.TabIndex = 16;
+            // 
+            // modeCombo
+            // 
+            this.modeCombo.FormattingEnabled = true;
+            this.modeCombo.Location = new System.Drawing.Point(12, 51);
+            this.modeCombo.Name = "modeCombo";
+            this.modeCombo.Size = new System.Drawing.Size(186, 21);
+            this.modeCombo.TabIndex = 17;
+            this.modeCombo.SelectedIndexChanged += new System.EventHandler(this.modeCombo_SelectedIndexChanged);
+            // 
+            // modeLabel
+            // 
+            this.modeLabel.AutoSize = true;
+            this.modeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modeLabel.Location = new System.Drawing.Point(52, 15);
+            this.modeLabel.Name = "modeLabel";
+            this.modeLabel.Size = new System.Drawing.Size(91, 33);
+            this.modeLabel.TabIndex = 18;
+            this.modeLabel.Text = "Mode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1398, 561);
+            this.Controls.Add(this.modeLabel);
+            this.Controls.Add(this.modeCombo);
+            this.Controls.Add(this.yText);
+            this.Controls.Add(this.xText);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.cameraButton);
+            this.Controls.Add(this.cameraCollection);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.grabBox);
+            this.Controls.Add(this.GrabLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.Label GrabLabel;
+        private System.Windows.Forms.TextBox grabBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cameraCollection;
+        private System.Windows.Forms.Button cameraButton;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.TextBox xText;
+        private System.Windows.Forms.TextBox yText;
+        private System.Windows.Forms.ComboBox modeCombo;
+        private System.Windows.Forms.Label modeLabel;
+    }
+}
+
---
+++ MyLaserTurret/MyLaserTurret/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Leap;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
+
+namespace MyLaserTurret
+{
+    public partial class Form1 : Form, ILeapEventDelegate {
+
+        // Setting up class variables
+        private Controller controller;                                                                                      // Leap motion controller
+        private LeapEventListener listener;                                                                                 // Listener for events thrown by Leap motion
+        private string[] modes = new string[] { "manual", "automatic" };                                                    // Available modes for turret
+        private string currentMode = "manual";                                                                              // Default mode when starting
+        private CascadeClassifier faceCascade = new CascadeClassifier("./detection/haarcascade_frontalface_default.xml");   // Data on for face detection
+        private FilterInfoCollection filter;                                                                                // Used to get all video devices
+        private VideoCaptureDevice videoCaptureDevice;                                                                      // Hold which video device is in use
+        private Stopwatch watch { get; set; }                                                                               // Used for rate limiting
+
+        // Constructor for the class
+        public Form1() {
+            watch = Stopwatch.StartNew();
+            InitializeComponent();
+            this.controller = new Controller();
+            this.listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter) {
+                cameraCollection.Items.Add(device.Name);
+            }
+            cameraCollection.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        // Event which is called when the form is loaded
+        private void Form1_Load(object sender, EventArgs e) {
+            port.Open();
+            modeCombo.Items.AddRange(modes);
+            modeCombo.SelectedIndex = 0;
+        }
+
+        // multi threaing function for invoking events notifications later [Abstract implementation Provided by Leap API]
+        delegate void LeapEventDelegate(string EventName);
+
+        // Handles all the LeapMotion events that are needed to function 
+        public void LeapEventNotification(string EventName) {
+            if (!this.InvokeRequired) {
+                switch (EventName) {
+                    case "onInit":
+                        Debug.WriteLine("Init");
+                        break;
+                    case "onConnect":
+                        Debug.WriteLine("Leap MotionConnected");
+                        break;
+                    case "onFrame":
+                        detectHandPosition(this.controller.Frame());
+                        break;
+                }
+            } else {
+                BeginInvoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+
+        // when in manual mode detect if the right hand is available.
+        // Get coords of the hand map them so arduino can understand and send coords to arduino
+        // Check to see if hand in grabbing, if so shoot the laser
+        public void detectHandPosition(Frame frame) {
+            if (currentMode == "automatic") return;
+            if (watch.ElapsedMilliseconds > 30) {
+                watch = Stopwatch.StartNew();
+                Hand hand = frame.Hands.Frontmost;
+                if (!hand.IsValid) return;
+                if (hand.IsLeft) return;
+                InteractionBox iBox = frame.InteractionBox;
+                Finger finger = hand.Fingers[1];
+                Vector leapPoint = finger.StabilizedTipPosition;
+                Vector normalizedPoint = iBox.NormalizePoint(leapPoint, false);
+
+                bool isGrabbing = hand.GrabStrength == 1;
+
+                grabBox.Text = isGrabbing.ToString();
+
+                if (isGrabbing) {
+                    port.Write("Shoot");
+                    return;
+                }
+
+                float appX = normalizedPoint.x * 180;
+                float appY = (1 - normalizedPoint.y) * 180;
+
+                float mappedX = 180 - ExtensionMethods.Map(appX, -500, 500, 0, 180);
+                float mappedY = 180 - ExtensionMethods.Map(appY, -160, 200, 0, 180);
+
+                xText.Text = ((int)mappedX).ToString();
+                yText.Text = ((int)mappedY).ToString();
+                port.Write(String.Format("X{0}Y{1}", (int)mappedX, (int)mappedY));
+
+            }
+        }
+
+        // Used to switch between camera sources during automatic mode
+        private void cameraButton_Click(object sender, EventArgs e) {
+            if (currentMode == "automatic") {
+                videoCaptureDevice = new VideoCaptureDevice(filter[cameraCollection.SelectedIndex].MonikerString);
+                videoCaptureDevice.NewFrame += Device_NewFrame;
+                videoCaptureDevice.Start();
+            }
+        }
+
+        // update method for when a new frame is rendered on the PictureBox
+        // Takes the frame, converts it to an image
+        // Runs the OpenCV model to find a face
+        // Gets the coordinates of the first rectangle and sends the coords to the arsuino
+        private void Device_NewFrame(object sender, NewFrameEventArgs EventArgs) {
+            Bitmap bitmap = (Bitmap)EventArgs.Frame.Clone();
+            Image<Bgr, byte> greyImage = new Image<Bgr, byte>(bitmap);
+
+            Rectangle[] rectangles = faceCascade.DetectMultiScale(greyImage, 1.1, 1, minSize: new Size(150, 150));
+            if (rectangles.Length == 0) return;
+
+            float mappedX = 180 - ExtensionMethods.Map(rectangles[0].X, 0, 500, 0, 180);
+            float mappedY = ExtensionMethods.Map(rectangles[0].Y, 0, 300, 60, 180);
+
+            xText.Invoke(() => xText.Text = ((int)mappedX).ToString());
+            yText.Invoke(() => yText.Text = ((int)mappedY).ToString());
+            port.Write(String.Format("X{0}Y{1}", (int)mappedX, (int)mappedY));
+            foreach (Rectangle rectangle in rectangles) {
+                using (Graphics graphics = Graphics.FromImage(bitmap)) {
+                    using (Pen pen = new Pen(Color.Red, 1)) {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        // When closing the application stop / switch off the camera
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            if (videoCaptureDevice.IsRunning) {
+                videoCaptureDevice.Stop();
+            }
+        }
+
+        // When the drop down for modes is changed
+        // Update the necessary variables
+        // If switched to manual mode stop the camera and remove the frozen image
+        private void modeCombo_SelectedIndexChanged(object sender, EventArgs e) {
+            ComboBox comboBox = (ComboBox)sender;
+            string selectedMode = (string)comboBox.SelectedItem;
+            currentMode = selectedMode;
+            if (selectedMode == "manual") {
+                if (videoCaptureDevice.IsRunning) {
+                    videoCaptureDevice.Stop();
+                    pictureBox1.Image.Dispose();
+                    pictureBox1.Image = null;
+                }
+            }
+        }
+    }
+
+    // Helper functions to allow for multi threaded updates
+    public static class ExtensionMethods {
+        public static float Map(float value, int fromLow, int fromHigh, int toLow, int toHigh) {
+            return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        }
+
+        public static void Invoke(this Control control, MethodInvoker mi) {
+            control.Invoke(mi);
+            return;
+        }
+    }
+
+    // Interface that is required by Leap Motion API
+    public interface ILeapEventDelegate {
+        void LeapEventNotification(string EventName);
+    }
+
+    // Listener class that is required by Leap Motion API
+    public class LeapEventListener : Listener {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventListener(ILeapEventDelegate delegateObject) {
+            this.eventDelegate = delegateObject;
+        }
+        public override void OnInit(Controller controller) {
+            this.eventDelegate.LeapEventNotification("onInit");
+        }
+        public override void OnConnect(Controller controller) {
+            this.eventDelegate.LeapEventNotification("onConnect");
+        }
+        public override void OnFrame(Controller controller) {
+            this.eventDelegate.LeapEventNotification("onFrame");
+        }
+        public override void OnExit(Controller controller) {
+            this.eventDelegate.LeapEventNotification("onExit");
+        }
+        public override void OnDisconnect(Controller controller) {
+            this.eventDelegate.LeapEventNotification("onDisconnect");
+        }
+    }
+}
